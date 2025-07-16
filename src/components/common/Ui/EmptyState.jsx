68d3d55ace12ef42d4d7a8ed0d78b394d25ce1cf
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-07-16 21:17:29 +0600

    text center the empty state component content

--- src/components/common/Ui/EmptyState.jsx
+++ src/components/common/Ui/EmptyState.jsx
@@ -10,8 +10,12 @@ const EmptyState = ({
 }) => {
   return (
     <div className={`flex flex-col items-center justify-center ${className} `}>
-      <h2 className="text-3xl font-semibold">{title}</h2>
-      {description && <p className="mt-3 font-semibold">{description}</p>}
+      <h2 className="text-2xl md:text-3xl text-center font-semibold">
+        {title}
+      </h2>
+      {description && (
+        <p className="mt-3 text-center font-semibold">{description}</p>
+      )}
       {btnLink && btnText && (
         <Link to={btnLink}>
           <button className="btn mt-4 btn-primary">
